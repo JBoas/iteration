54358fd9f3f6a44aee3dc031150cb15bbaa2176c
JBoas <jboas@sjnma.org>
2017-10-30 14:35:56 -0500

    not done

--- problem4.py
+++ problem4.py
@@ -1,12 +1,33 @@
 '''
 -get input sentence from user
+-determine " " in the sentence
 -seperate words from sentence
 -combine words to look like the .split function
+
+
+
+
+sentence = input("Enter sentence")
+finalsentence = []
+
+while True:
+	for space in sentence:
+		if space == " ":
+
+		else:
+			print("n")
+
 '''
+sentence = input("Enter Sentence ")
+words = []
+wordstart = 0
+finalwords = []
 
-sentence = input("Enter a sentence")
-splitsentence = []
+for x in range(0,(len(sentence))):
+	if x == " ":
+		words.append(sentence[wordstart:x])
+		wordstart = x + 1
 
-for word in sentence:
-	splitsentence.append([word])
-print(splitsentence)
+if x > wordstart:
+	words.append(sentence[wordstart:len(sentence)])
+
---
+++ problem4.py.save
@@ -0,0 +1,12 @@
+'''
+-get input sentence from user
+-seperate words from sentence
+-combine words to look like the .split function
+'''
+
+sentence = input("Enter a sentence")
+splitsentence = []
+
+for word in sentence:
+	splitsentence.append([word])
+print(splitsentence)
