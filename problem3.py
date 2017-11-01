7ad62126fd6216a2e4da13568758c76facd26cbc
JBoas <jboas@sjnma.org>
2017-11-01 14:10:18 -0500

    x

---
+++ problem3.py
@@ -0,0 +1,13 @@
+import random
+'''
+-Tell user to think of number
+-develop a range of numbers 1-1000
+-Guess a number 1-1000
+-have the user say higher or lower to the number
+-guess a number according to user input
+'''
+print("Think of a number and tell me to guess higher or lower. If I got the number right then input 1 ")
+
+max = 1000
+min = 1
+guess1 = 500
--- problem4.py
+++ problem4.py
@@ -1,33 +1,28 @@
 '''
--get input sentence from user
--determine " " in the sentence
--seperate words from sentence
--combine words to look like the .split function
-
-
-
-
-sentence = input("Enter sentence")
-finalsentence = []
-
-while True:
-	for space in sentence:
-		if space == " ":
-
-		else:
-			print("n")
+-get input sentence from user-----
+-determine " " in the sentence-----
+-seperate words from sentence-----
+-combine words to look like the .split function-----
 
 '''
-sentence = input("Enter Sentence ")
+
+
+sentence = input("Enter Sentence ") + " "
 words = []
 wordstart = 0
-finalwords = []
+n = 0
 
-for x in range(0,(len(sentence))):
-	if x == " ":
-		words.append(sentence[wordstart:x])
-		wordstart = x + 1
+for x in range(0,len(sentence)):
+	if( x != len(sentence)):
 
-if x > wordstart:
-	words.append(sentence[wordstart:len(sentence)])
+		if (sentence[x] == " " and sentence[x - 1] == " "):
+			continue
+		elif (sentence[x] == " "):
+			words.append(sentence[wordstart:x])
+			wordstart = x + 1
+			n = x
+		while(sentence[n] == " ") and (sentence[n-1] == " "):
+			wordstart = x + 1
 
+
+print (words)
