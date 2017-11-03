a7f974ea46d2564992ee6f18c95585be05ab0354
JBoas <jboas@sjnma.org>
2017-11-03 11:46:29 -0500

    complete

--- problem3.py
+++ problem3.py
@@ -6,8 +6,25 @@ import random
 -have the user say higher or lower to the number
 -guess a number according to user input
 '''
-print("Think of a number and tell me to guess higher or lower. If I got the number right then input 1 ")
+print("Think of a number and tell me to guess higher or lower. If I got the number right then input You win ")
 
 max = 1000
 min = 1
-guess1 = 500
+guessnumber = 500
+
+while True:
+	print("type too low or too high if my guess is to low or high!")
+	print(guessnumber)
+	userinput = input("higher or lower? ")
+	if(userinput == "too high" or "Too high"):
+		max = guessnumber
+		guessnumber = (((max - min)/2)+ min)
+		print(guessnumber)
+
+	elif(userinput == "too low" or "Too low"):
+		min = guessnumber
+		guessnumber = (((max - min)/2)+ min)
+		print(guessnumber)
+
+if(userinput == "You win"):
+	print("I win!")
